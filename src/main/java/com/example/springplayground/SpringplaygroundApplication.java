8759a470c429c30e41a8a58050f1db20e89a2b57
mohamed <moh4med.ahmed@gmail.com>
2021-10-29 16:43:56 +0200

    add upload files

--- src/main/java/com/example/springplayground/SpringplaygroundApplication.java
+++ src/main/java/com/example/springplayground/SpringplaygroundApplication.java
@@ -1,13 +1,26 @@
 package com.example.springplayground;
 
+import com.example.springplayground.storage.StorageProperties;
+import com.example.springplayground.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class SpringplaygroundApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringplaygroundApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
