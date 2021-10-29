d0fb165032f13cc40697e9a08c26b8a560c8c0b6
mohamed <moh4med.ahmed@gmail.com>
2021-10-29 15:39:36 +0200

    consuming quote rest service

--- src/main/java/com/example/springplayground/SpringplaygroundApplication.java
+++ src/main/java/com/example/springplayground/SpringplaygroundApplication.java
@@ -1,13 +1,36 @@
 package com.example.springplayground;
 
+import com.example.springplayground.domain.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringplaygroundApplication {
+    private static final Logger log = LoggerFactory.getLogger(SpringplaygroundApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringplaygroundApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "https://quoters.apps.pcfone.io/api/random", Quote.class);
+            if (quote != null) {
+                log.info(quote.toString());
+            }
+        };
+    }
 }
